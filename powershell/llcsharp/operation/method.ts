650d5fa042182b3d830717111fda665c813cb3e1
Microsoft Graph DevX Tooling <GraphTooling@service.microsoft.com>
2025-02-18 21:27:04 +0300

    Default is always an object

--- powershell/llcsharp/operation/method.ts
+++ powershell/llcsharp/operation/method.ts
@@ -351,16 +351,7 @@ export class OperationMethod extends Method {
           yield 'request.Content.Headers.ContentType = System.Net.Http.Headers.MediaTypeHeaderValue.Parse(!string.IsNullOrEmpty(contentType)? contentType : mimeType);';
         } else {
 
-          if (bp.typeDeclaration.schema.type === 'object' && (method.capitalize() === 'Post' || method.capitalize() === 'Put' || method.capitalize() === 'Patch')) {
-            yield 'string cleanedBody = "@{}";';
-            yield 'if (body != null) {';
-            yield '    cleanedBody = body.ToJson(null).ToString();';
-            yield '    cleanedBody = Microsoft.Graph.PowerShell.JsonUtilities.JsonExtensions.ReplaceAndRemoveSlashes(cleanedBody);';
-            yield '    Newtonsoft.Json.Linq.JObject jsonObject = Newtonsoft.Json.Linq.JObject.Parse(cleanedBody);';
-            yield '    cleanedBody = Microsoft.Graph.PowerShell.JsonUtilities.JsonExtensions.RemoveDefaultNullProperties(jsonObject);';
-            yield '}';
-            yield EOL;
-          } else if (bp.typeDeclaration.schema.type === 'array' && (method.capitalize() === 'Post' || method.capitalize() === 'Put' || method.capitalize() === 'Patch')) {
+          if (bp.typeDeclaration.schema.type === 'array' && (method.capitalize() === 'Post' || method.capitalize() === 'Put' || method.capitalize() === 'Patch')) {
             yield 'string cleanedBody = "@[{}]";';
             yield 'if (body != null) {';
             yield '    cleanedBody = new Microsoft.Graph.PowerShell.Runtime.Json.XNodeArray(global::System.Linq.Enumerable.ToArray(System.Linq.Enumerable.Select(body, (__x) => __x?.ToJson(null, Microsoft.Graph.PowerShell.Runtime.SerializationMode.None)))).ToString();';
@@ -369,6 +360,17 @@ export class OperationMethod extends Method {
             yield '    cleanedBody = Microsoft.Graph.PowerShell.JsonUtilities.JsonExtensions.RemoveDefaultNullProperties(jsonArray);';
             yield '}';
             yield EOL;
+          } else {
+            if (method.capitalize() === 'Post' || method.capitalize() === 'Put' || method.capitalize() === 'Patch') {
+              yield 'string cleanedBody = "@{}";';
+              yield 'if (body != null) {';
+              yield '    cleanedBody = body.ToJson(null).ToString();';
+              yield '    cleanedBody = Microsoft.Graph.PowerShell.JsonUtilities.JsonExtensions.ReplaceAndRemoveSlashes(cleanedBody);';
+              yield '    Newtonsoft.Json.Linq.JObject jsonObject = Newtonsoft.Json.Linq.JObject.Parse(cleanedBody);';
+              yield '    cleanedBody = Microsoft.Graph.PowerShell.JsonUtilities.JsonExtensions.RemoveDefaultNullProperties(jsonObject);';
+              yield '}';
+              yield EOL;
+            }
           }
           yield '// set body content';
           yield `request.Content = ${bp.serializeToContent(bp.mediaType, ClientRuntime.SerializationMode.None)};`;
